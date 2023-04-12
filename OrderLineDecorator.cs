603a08cb4730dba2cd4f614b0a71ad652b61836a
Gem Immanuel <gemify@gmail.com>
2023-04-12 14:01:17 -0400

    Uses font and size set on the chart

--- OrderLineDecorator.cs
+++ OrderLineDecorator.cs
@@ -1,32 +1,25 @@
 #region Using declarations
+using NinjaTrader.Cbi;
+using NinjaTrader.CQG.ProtoBuf;
+using NinjaTrader.Gui;
+using NinjaTrader.Gui.Chart;
+using NinjaTrader.Gui.Tools;
+using NinjaTrader.NinjaScript.Indicators.Gemify;
 using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.ComponentModel.DataAnnotations;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows;
-using System.Windows.Input;
 using System.Windows.Media;
 using System.Xml.Serialization;
-using NinjaTrader.Cbi;
-using NinjaTrader.Gui;
-using NinjaTrader.Gui.Chart;
-using NinjaTrader.Gui.SuperDom;
-using NinjaTrader.Gui.Tools;
-using NinjaTrader.Data;
-using NinjaTrader.NinjaScript;
-using NinjaTrader.Core.FloatingPoint;
-using NinjaTrader.NinjaScript.DrawingTools;
-using System.Windows.Media.Media3D;
-using NinjaTrader.NinjaScript.Indicators.Gemify;
+using Account = NinjaTrader.Cbi.Account;
+using Order = NinjaTrader.Cbi.Order;
 #endregion
 
 //This namespace holds Indicators in this folder and is required. Do not change it. 
 namespace NinjaTrader.NinjaScript.Indicators.Gemify
 {
-    public enum OrderLineDecoratorDisplayValue
+	public enum OrderLineDecoratorDisplayValue
     {
         TICKS,
         CURRENCY,
@@ -34,24 +27,38 @@ namespace NinjaTrader.NinjaScript.Indicators.Gemify
     }
 
     public class OrderLineDecorator : Indicator
-	{		
+	{
+        // ---------------------------------------
+		// Update this list as you see fit
+		// to support additional order types
+		// categorized as either STOP or TARGET
+        // ---------------------------------------
+        private List<OrderType> RecognizedStopOrderTypes = new List<OrderType>() { OrderType.StopMarket, OrderType.StopLimit, OrderType.MIT };
+        private List<OrderType> RecognizedTargetOrderTypes = new List<OrderType>() { OrderType.Limit };
+
         // For simplicity, we'll bunch orders as either stops or targets
-		protected enum OrderLineDecoratorOrderType
+        protected enum OrderLineDecoratorOrderType
         {
             STOP,
-            TARGET
+            TARGET,
+			OTHER
         }
 
         class OrderTypeAndText
 		{ 
 			public OrderLineDecoratorOrderType orderType;
             public String text;
+			public int nOrderTypes;
 		}
 
 		private Account gAccount;
 		private AccountSelector gAccountSelector;
 		private Dictionary<string, int> orderQtyTracker;
         private Dictionary<double, OrderTypeAndText> toRender;
+
+        private const String sampleOrderLabelText = "XXXXXXXX XXX XXXX";
+		private SimpleFont chartFont;
+		private float sampleOrderLabelTextWidth;
 
         private SharpDX.DirectWrite.TextFormat textFormat;
 
@@ -82,9 +89,8 @@ namespace NinjaTrader.NinjaScript.Indicators.Gemify
 				IsSuspendedWhileInactive = true;
 
 				// Default values
-				IsDebug = true;
-                Font = new SimpleFont("Arial", 12);
-				DisplayMode = OrderLineDecoratorDisplayValue.BOTH;
+				IsDebug = false;
+                DisplayMode = OrderLineDecoratorDisplayValue.BOTH;
 
 				StopFillBrush = Brushes.Maroon;
 				TargetFillBrush = Brushes.DarkGreen;
@@ -100,11 +106,25 @@ namespace NinjaTrader.NinjaScript.Indicators.Gemify
             }
 			else if (State == State.DataLoaded)
 			{
-                textFormat = Font.ToDirectWriteTextFormat();
+				SetTextFont(ChartControl.Properties.LabelFont);
+                sampleOrderLabelTextWidth = CalculateLabelSize(chartFont, sampleOrderLabelText);
             }
-		}
+        }
 
-		protected override void OnBarUpdate()
+		private float CalculateLabelSize(SimpleFont font, String label)
+		{
+            SharpDX.DirectWrite.TextFormat textFormat = font.ToDirectWriteTextFormat();
+            return new SharpDX.DirectWrite.TextLayout(NinjaTrader.Core.Globals.DirectWriteFactory, label, textFormat, ChartPanel.X + ChartPanel.W, textFormat.FontSize).Metrics.Width;
+
+        }
+
+		private void SetTextFont(SimpleFont font)
+		{
+            chartFont = font;
+            textFormat = chartFont.ToDirectWriteTextFormat();
+        }
+
+        protected override void OnBarUpdate()
 		{
 			// We only care about realtime positions
 			if (State == State.Historical) return;
@@ -151,17 +171,20 @@ namespace NinjaTrader.NinjaScript.Indicators.Gemify
 								Debug("Unsupported order type. [" + order.OrderType + "] Skipping.");
 								continue;
 							}
-							string key = orderPrice + order.OrderType.ToString();
+
+							string key = orderPrice + GetOrderLineDecoratorOrderType(order.OrderType).ToString();
 
 							// Attempt to count orders of the same type and same price 
 							if (orderQtyTracker.ContainsKey(key))
 							{
-								orderQtyTracker[key] = orderQtyTracker[key] + order.Quantity;
+								orderQtyTracker[key] += order.Quantity;
+								if (orderPrice == 12822) Print("Updating " + orderPrice + ", " + order.Quantity + ", " + order.Oco + ", " + order.OrderType);
 							}
 							else
 							{
 								orderQtyTracker.Add(key, order.Quantity);
-							}
+                                if (orderPrice == 12822) Print("ADD " + orderPrice + ", " + order.Quantity + ", " + order.Oco + ", " + order.OrderType);
+                            }
 
 							// Use aggregated order quantity
 							int orderQty = orderQtyTracker[key];
@@ -172,15 +195,15 @@ namespace NinjaTrader.NinjaScript.Indicators.Gemify
 							double currencyValue = priceDiff * Instrument.MasterInstrument.PointValue * orderQty;
 
 							// Generate text for decoration
-							string orderType = order.IsStopLimit || order.IsStopMarket ? "STOP" : "TARGET";
+							string orderType = IsStopOrder(order) ? "STOP" : "TARGET";
 							string text = orderType + " (" + orderQty + ") " + (
-                                (DisplayMode == OrderLineDecoratorDisplayValue.TICKS || DisplayMode == OrderLineDecoratorDisplayValue.BOTH ? ((order.IsStopLimit || order.IsStopMarket) && ticks > 0 ? "+" : "") + ticks + " ticks" : "") +
+                                (DisplayMode == OrderLineDecoratorDisplayValue.TICKS || DisplayMode == OrderLineDecoratorDisplayValue.BOTH ? (IsStopOrder(order) && ticks > 0 ? "+" : "") + ticks + " ticks" : "") +
                                 (DisplayMode == OrderLineDecoratorDisplayValue.BOTH ? " : " : "") + 
                                 (DisplayMode == OrderLineDecoratorDisplayValue.CURRENCY || DisplayMode == OrderLineDecoratorDisplayValue.BOTH ? currencyValue.ToString("C2") : "")
 								);
 
 							// Store order type and text against order price. This will be picked up and rendered by the OnRender call
-							toRender[orderPrice] = new OrderTypeAndText() { orderType = (order.IsStopLimit || order.IsStopMarket ? OrderLineDecoratorOrderType.STOP : OrderLineDecoratorOrderType.TARGET), text = text };
+							toRender[orderPrice] = new OrderTypeAndText() { orderType = GetOrderLineDecoratorOrderType(order.OrderType), text = text };
 						}
 					}
 				}
@@ -190,14 +213,37 @@ namespace NinjaTrader.NinjaScript.Indicators.Gemify
             ForceRefresh();
         }
 
-        private double GetOrderPrice(Order order)
+		private bool IsStopOrder(Order order)
+		{
+			return RecognizedStopOrderTypes.Contains(order.OrderType);
+        }
+
+        private bool IsTargetOrder(Order order)
+        {
+            return RecognizedTargetOrderTypes.Contains(order.OrderType);
+        }
+
+        private OrderLineDecoratorOrderType GetOrderLineDecoratorOrderType(OrderType orderType)
+		{
+            if (RecognizedStopOrderTypes.Contains(orderType))
+			{
+				return OrderLineDecoratorOrderType.STOP;
+			}
+            else if (RecognizedTargetOrderTypes.Contains(orderType))
+            {
+				return OrderLineDecoratorOrderType.TARGET;
+			}
+			else
+				return OrderLineDecoratorOrderType.OTHER;
+		}
+		private double GetOrderPrice(Order order)
 		{
 			double orderPrice = 0;
 
-			// Only operates on Stop Limits, Stop Markets and Market if Touched - used as stops
-			if (order.IsStopLimit || order.IsStopMarket || order.IsMarketIfTouched) orderPrice = order.StopPrice;
-			// and Limit exits
-			else if (order.IsLimit) orderPrice = order.LimitPrice;
+            // Only operates on recognized stop types
+            if (RecognizedStopOrderTypes.Contains(order.OrderType)) orderPrice = order.StopPrice;
+			// and target exits
+			else if (RecognizedTargetOrderTypes.Contains(order.OrderType)) orderPrice = order.LimitPrice;
 
 			return orderPrice;
 
@@ -207,10 +253,12 @@ namespace NinjaTrader.NinjaScript.Indicators.Gemify
         {
 			if (State == State.Historical || toRender.IsNullOrEmpty()) return;
 
-			// TODO:
-			// Need to figure out how to calculate the offset given the OrderDisplayBarLength property.
-			float THIS_VAR_IS_DRIVING_ME_NUTS = 190;
-
+			if (ChartControl.Properties.LabelFont != chartFont)
+			{
+				SetTextFont(ChartControl.Properties.LabelFont);
+                sampleOrderLabelTextWidth = CalculateLabelSize(chartFont, sampleOrderLabelText);
+			}
+            
             using (SharpDX.Direct2D1.Brush borderBrushDx = OutlineBrush.ToDxBrush(RenderTarget))
             using (SharpDX.Direct2D1.Brush stopBrushDx = StopFillBrush.ToDxBrush(RenderTarget))
             using (SharpDX.Direct2D1.Brush targetBrushDx = TargetFillBrush.ToDxBrush(RenderTarget))
@@ -218,12 +266,14 @@ namespace NinjaTrader.NinjaScript.Indicators.Gemify
             {
                 foreach (KeyValuePair<double, OrderTypeAndText> kvp in toRender)
 				{
-					SharpDX.DirectWrite.TextLayout textLayout = new SharpDX.DirectWrite.TextLayout(NinjaTrader.Core.Globals.DirectWriteFactory, kvp.Value.text, textFormat, ChartPanel.X + ChartPanel.W, textFormat.FontSize);
+					SharpDX.DirectWrite.TextLayout textLayout = new SharpDX.DirectWrite.TextLayout(NinjaTrader.Core.Globals.DirectWriteFactory, kvp.Value.text + "", textFormat, ChartPanel.X + ChartPanel.W, textFormat.FontSize);
 
                     float textWidth = textLayout.Metrics.Width;
 					float textHeight = textLayout.Metrics.Height;
 
-                    float x = (float)(ChartPanel.W - (ChartPanel.W * ((ChartControl.OwnerChart.ChartTrader.Properties.OrderDisplayBarLength)/100.0)) - textWidth - THIS_VAR_IS_DRIVING_ME_NUTS);
+					// Print(kvp.Value.text + ": " + textWidth);
+
+                    float x = (float)(ChartPanel.W - ((ChartPanel.W * (ChartControl.OwnerChart.ChartTrader.Properties.OrderDisplayBarLength/100.0)) + textWidth + sampleOrderLabelTextWidth + 75));
 					int priceCoordinate = chartScale.GetYByValue(kvp.Key);
 					float y = priceCoordinate - ((textHeight + 7) / 2);
 
@@ -245,10 +295,6 @@ namespace NinjaTrader.NinjaScript.Indicators.Gemify
         }
 
         #region Parameters
-        [NinjaScriptProperty]
-        [Display(Name = "Font", GroupName = "Parameters", Order = 100)]
-        public SimpleFont Font
-        { get; set; }
 
         [NinjaScriptProperty]
         [Display(Name = "Display", GroupName = "Parameters", Order = 200)]
@@ -320,18 +366,18 @@ namespace NinjaTrader.NinjaScript.Indicators
 	public partial class Indicator : NinjaTrader.Gui.NinjaScript.IndicatorRenderBase
 	{
 		private Gemify.OrderLineDecorator[] cacheOrderLineDecorator;
-		public Gemify.OrderLineDecorator OrderLineDecorator(SimpleFont font, OrderLineDecoratorDisplayValue displayMode, Brush stopFillBrush, Brush targetFillBrush, Brush outlineBrush, Brush textBrush)
+		public Gemify.OrderLineDecorator OrderLineDecorator(OrderLineDecoratorDisplayValue displayMode, Brush stopFillBrush, Brush targetFillBrush, Brush outlineBrush, Brush textBrush)
 		{
-			return OrderLineDecorator(Input, font, displayMode, stopFillBrush, targetFillBrush, outlineBrush, textBrush);
+			return OrderLineDecorator(Input, displayMode, stopFillBrush, targetFillBrush, outlineBrush, textBrush);
 		}
 
-		public Gemify.OrderLineDecorator OrderLineDecorator(ISeries<double> input, SimpleFont font, OrderLineDecoratorDisplayValue displayMode, Brush stopFillBrush, Brush targetFillBrush, Brush outlineBrush, Brush textBrush)
+		public Gemify.OrderLineDecorator OrderLineDecorator(ISeries<double> input, OrderLineDecoratorDisplayValue displayMode, Brush stopFillBrush, Brush targetFillBrush, Brush outlineBrush, Brush textBrush)
 		{
 			if (cacheOrderLineDecorator != null)
 				for (int idx = 0; idx < cacheOrderLineDecorator.Length; idx++)
-					if (cacheOrderLineDecorator[idx] != null && cacheOrderLineDecorator[idx].Font == font && cacheOrderLineDecorator[idx].DisplayMode == displayMode && cacheOrderLineDecorator[idx].StopFillBrush == stopFillBrush && cacheOrderLineDecorator[idx].TargetFillBrush == targetFillBrush && cacheOrderLineDecorator[idx].OutlineBrush == outlineBrush && cacheOrderLineDecorator[idx].TextBrush == textBrush && cacheOrderLineDecorator[idx].EqualsInput(input))
+					if (cacheOrderLineDecorator[idx] != null && cacheOrderLineDecorator[idx].DisplayMode == displayMode && cacheOrderLineDecorator[idx].StopFillBrush == stopFillBrush && cacheOrderLineDecorator[idx].TargetFillBrush == targetFillBrush && cacheOrderLineDecorator[idx].OutlineBrush == outlineBrush && cacheOrderLineDecorator[idx].TextBrush == textBrush && cacheOrderLineDecorator[idx].EqualsInput(input))
 						return cacheOrderLineDecorator[idx];
-			return CacheIndicator<Gemify.OrderLineDecorator>(new Gemify.OrderLineDecorator(){ Font = font, DisplayMode = displayMode, StopFillBrush = stopFillBrush, TargetFillBrush = targetFillBrush, OutlineBrush = outlineBrush, TextBrush = textBrush }, input, ref cacheOrderLineDecorator);
+			return CacheIndicator<Gemify.OrderLineDecorator>(new Gemify.OrderLineDecorator(){ DisplayMode = displayMode, StopFillBrush = stopFillBrush, TargetFillBrush = targetFillBrush, OutlineBrush = outlineBrush, TextBrush = textBrush }, input, ref cacheOrderLineDecorator);
 		}
 	}
 }
@@ -340,14 +386,14 @@ namespace NinjaTrader.NinjaScript.MarketAnalyzerColumns
 {
 	public partial class MarketAnalyzerColumn : MarketAnalyzerColumnBase
 	{
-		public Indicators.Gemify.OrderLineDecorator OrderLineDecorator(SimpleFont font, OrderLineDecoratorDisplayValue displayMode, Brush stopFillBrush, Brush targetFillBrush, Brush outlineBrush, Brush textBrush)
+		public Indicators.Gemify.OrderLineDecorator OrderLineDecorator(OrderLineDecoratorDisplayValue displayMode, Brush stopFillBrush, Brush targetFillBrush, Brush outlineBrush, Brush textBrush)
 		{
-			return indicator.OrderLineDecorator(Input, font, displayMode, stopFillBrush, targetFillBrush, outlineBrush, textBrush);
+			return indicator.OrderLineDecorator(Input, displayMode, stopFillBrush, targetFillBrush, outlineBrush, textBrush);
 		}
 
-		public Indicators.Gemify.OrderLineDecorator OrderLineDecorator(ISeries<double> input , SimpleFont font, OrderLineDecoratorDisplayValue displayMode, Brush stopFillBrush, Brush targetFillBrush, Brush outlineBrush, Brush textBrush)
+		public Indicators.Gemify.OrderLineDecorator OrderLineDecorator(ISeries<double> input , OrderLineDecoratorDisplayValue displayMode, Brush stopFillBrush, Brush targetFillBrush, Brush outlineBrush, Brush textBrush)
 		{
-			return indicator.OrderLineDecorator(input, font, displayMode, stopFillBrush, targetFillBrush, outlineBrush, textBrush);
+			return indicator.OrderLineDecorator(input, displayMode, stopFillBrush, targetFillBrush, outlineBrush, textBrush);
 		}
 	}
 }
@@ -356,14 +402,14 @@ namespace NinjaTrader.NinjaScript.Strategies
 {
 	public partial class Strategy : NinjaTrader.Gui.NinjaScript.StrategyRenderBase
 	{
-		public Indicators.Gemify.OrderLineDecorator OrderLineDecorator(SimpleFont font, OrderLineDecoratorDisplayValue displayMode, Brush stopFillBrush, Brush targetFillBrush, Brush outlineBrush, Brush textBrush)
+		public Indicators.Gemify.OrderLineDecorator OrderLineDecorator(OrderLineDecoratorDisplayValue displayMode, Brush stopFillBrush, Brush targetFillBrush, Brush outlineBrush, Brush textBrush)
 		{
-			return indicator.OrderLineDecorator(Input, font, displayMode, stopFillBrush, targetFillBrush, outlineBrush, textBrush);
+			return indicator.OrderLineDecorator(Input, displayMode, stopFillBrush, targetFillBrush, outlineBrush, textBrush);
 		}
 
-		public Indicators.Gemify.OrderLineDecorator OrderLineDecorator(ISeries<double> input , SimpleFont font, OrderLineDecoratorDisplayValue displayMode, Brush stopFillBrush, Brush targetFillBrush, Brush outlineBrush, Brush textBrush)
+		public Indicators.Gemify.OrderLineDecorator OrderLineDecorator(ISeries<double> input , OrderLineDecoratorDisplayValue displayMode, Brush stopFillBrush, Brush targetFillBrush, Brush outlineBrush, Brush textBrush)
 		{
-			return indicator.OrderLineDecorator(input, font, displayMode, stopFillBrush, targetFillBrush, outlineBrush, textBrush);
+			return indicator.OrderLineDecorator(input, displayMode, stopFillBrush, targetFillBrush, outlineBrush, textBrush);
 		}
 	}
 }
